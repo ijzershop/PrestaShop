edb5905c2c3f3ed0f42bd6bc2d11e95857a10ee4
JB Stoker <jelmer@ijzershop.nl>
2020-06-12 10:59:49 +0200

    added retour option in koopmans module, added switch order to customer option in order detail page

--- override/classes/pdf/PDFGenerator.php
+++ override/classes/pdf/PDFGenerator.php
@@ -1,7 +1,24 @@
 <?php
 class PDFGenerator extends PDFGeneratorCore
 {
+    /**
+     * @see TCPDF::Header()
+     */
+    public function Header()
+    {
+        $this->writeHTML($this->header);    
+    	
+    	if(strpos($this->header, 'TOEVOEGEN') !== false){
+        	$img_file = '../themes/modernesmid/assets/img/toevoegen_watermerk-min.png';
+        	$this->Image($img_file, 30, 40, 210, 297, '', '', '', false, 300, '', false, false, 0);
+    	}
 
+
+		if(strpos($this->header, 'AFHALEN') !== false){
+        	$img_file = '../themes/modernesmid/assets/img/afhalen_watermerk-min.png';
+        	$this->Image($img_file, 30, 40, 210, 297, '', '', '', false, 300, '', false, false, 0);
+    	}
+    }
 
     public function writePage()
     {

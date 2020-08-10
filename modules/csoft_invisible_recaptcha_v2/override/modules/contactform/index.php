38755a26f23dc77faffadf1c53adc0851960d631
JB Stoker <jelmer@ijzershop.nl>
2020-08-10 13:27:52 +0200

    module updates

---
+++ modules/csoft_invisible_recaptcha_v2/override/modules/contactform/index.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * csoft_invisible_recaptcha_v2 front-end module version 1.1.1 for Prestashop 1.7
+ * Support contact : prestashop@comonsoft.com.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is the property of Com'onSoft
+ * that is bundled with this package.
+ * It is also available through the world-wide-web at this URL:
+ * https://boutique.comonsoft.com/
+ *
+ * @category  front-end
+ * @package   csoft_invisible_recaptcha_v2
+ * @author    Com'onSoft (http://www.comonsoft.com/)
+ * @copyright 2016-2020. Com'onSoft and contributors
+ * @version   1.1.2
+ */
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+header('Location: ../');
+exit;
---
+++ modules/csoft_invisible_recaptcha_v2/translations/fr.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * csoft_invisible_recaptcha_v2 front-end module version 1.1.1 for Prestashop 1.7
+ * Support contact : prestashop@comonsoft.com.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is the property of Com'onSoft
+ * that is bundled with this package.
+ * It is also available through the world-wide-web at this URL:
+ * https://boutique.comonsoft.com/
+ *
+ * @category  front-end
+ * @package   csoft_invisible_recaptcha_v2
+ * @author    Com'onSoft (http://www.comonsoft.com/)
+ * @copyright 2016-2020. Com'onSoft and contributors
+ * @version   1.1.2
+ */
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_95f3ff3c125ae47e43b6afa408f86fc1'] = 'Invisible reCaptcha';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_2ad696f635d1329ee717b3e409903c33'] = 'Ajoutez un reCaptcha invisible à votre formulaire de contact et page d\'inscription client';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_a3635d4428d8cbcf6dccce8442e81505'] = 'a besoin d\'être configuré';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_43deb32873981edae3e1b412b595aaa7'] = 'La balise HTML button type=\"submit\" name=\"submitMessage\" est introuvable dans \"contact-form.tpl\"';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_894d6b2a94ab5d23b44b192286ee97b6'] = 'Le template \"contact-form.tpl\" n\'a pas été trouvé dans votre thème !';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_15cecc39d0a1bd801d2a60bdca3cbd59'] = 'La balise HTML input type=\"submit\" name=\"submitMessage\" est introuvable dans \"contactform.tpl\"';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_bd16046f9e4bfea5a03cdff213c7f8ec'] = 'Le template \"contactform.tpl\" n\'a pas été trouvé dans votre thème !';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_de7e39ae12eb7d5e08c84ff8739ee5fc'] = 'Veuillez remplir la clé publique captcha';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_30c1139b1b654780ec1ef5c35323806d'] = 'Veuillez remplir la clé privée captcha';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_c888438d14855d7d96a2724ee9c306bd'] = 'Paramètres mis à jour';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_2f90ba68e3387f62b0e22760c3bb9bd1'] = 'Configuration reCaptcha Invisible';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_77f682c46c4c98d39cfb703d3606f505'] = 'Pour obtenir vos propres clés publiques et privées, veuillez cliquer sur le lien suivant';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_059cff59cc5ead2a76d5d033c29a4754'] = 'Clé publique reCaptcha';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_c25f66f2154ee54e4a57ad5c9897a264'] = 'Clé privée reCaptcha';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_2aa54b500a188f2c2b83f47dfc00078c'] = 'Afficher le badge reCaptcha';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Activée';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_b9f5c797ebbf55adccdd8539a65a0241'] = 'Désactivé';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_20e25a0442a6ae51330986dae8a81866'] = 'Activer reCaptcha sur la page d\'inscription';
+$_MODULE['<{csoft_invisible_recaptcha_v2}prestashop>csoft_invisible_recaptcha_v2_c9cc8cce247e49bae79f15173ce97354'] = 'Sauvegarder';

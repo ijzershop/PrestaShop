3a89adb8e789762cca42120bfb8554f5e0955a8b
JB Stoker <jelmer@ijzershop.nl>
2020-09-18 10:04:22 +0200

    bugfixes

--- override/classes/Cookie.php
+++ override/classes/Cookie.php
@@ -2,7 +2,8 @@
 
 class Cookie extends CookieCore{
 
- protected function encryptAndSetCookie($cookie = null){
+ protected function encryptAndSetCookie($cookie = null)
+    {
         // Check if the content fits in the Cookie
         $length = (ini_get('mbstring.func_overload') & 2) ? mb_strlen($cookie, ini_get('default_charset')) : strlen($cookie);
         if ($length >= 1048576) {
@@ -15,25 +16,37 @@ class Cookie extends CookieCore{
             $content = 0;
             $time = 1;
         }
-
-        if (PHP_VERSION_ID < 70300) {
-           return setcookie($this->_name, $content, $time, '"'.$this->_path.'"; SameSite=Strict"', $this->_domain, $this->_secure, true);
+        
+        if (Configuration::get('PS_SSL_ENABLED') == 0 or strpos($_SERVER['REQUEST_URI'], 'admin') === 1){
+            return setcookie($this->_name, $content, $time, $this->_path, $this->_domain, $this->_secure, true);
+        }else{
+            if (PHP_VERSION_ID < 70300) {
+                return setcookie(
+                    $this->_name,
+                    $content,
+                    $time,
+                    $this->_path,
+                    $this->_domain . '; SameSite= None',
+                    $this->_secure,
+                    true
+                );
+            }else{
+                return setcookie(
+                    $this->_name,
+                    $content,
+                    [
+                        'expires' => $time,
+                        'path' => $this->_path,
+                        'domain' => $this->_domain,
+                        'secure' => $this->_secure,
+                        'httponly' => true,
+                        'samesite' => 'None',
+                    ]
+                );
+            }
+            
         }
-        else {
-           return setcookie($this->_name, $content, [
-                'expires' => $time,
-                'path' => $this->_path,
-                'domain' => $this->_domain,
-                'samesite' => 'Strict',
-                'secure' => $this->_secure,
-                'httponly' => true,
-            ]);
-        }
-
-
-
-
-        // return setcookie($this->_name, $content, $time, $this->_path, $this->_domain, $this->_secure, true);
+        
     }
 
 

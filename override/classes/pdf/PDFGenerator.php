48ae14329cbbdbed41395a804257424b7f491ab9
JB Stoker <jelmer@ijzershop.nl>
2020-11-13 10:38:08 +0100

    bugfix printing bulk pdf delivery slip

--- override/classes/pdf/PDFGenerator.php
+++ override/classes/pdf/PDFGenerator.php
@@ -6,8 +6,9 @@ class PDFGenerator extends PDFGeneratorCore
      */
     public function Header()
     {
-        $this->writeHTML($this->header);    
-    	
+
+        $this->writeHTML($this->header);
+
     	if(strpos($this->header, 'TOEVOEGEN') !== false){
         	$img_file = '../themes/modernesmid/assets/img/toevoegen_watermerk-min.png';
         	$this->Image($img_file, 30, 40, 210, 297, '', '', '', false, 300, '', false, false, 0);
@@ -21,8 +22,9 @@ class PDFGenerator extends PDFGeneratorCore
 
     }
 
-    public function writePage()
+    public function writePage($reference = null)
     {
+
         $barcodeStyle = array(
             'border' => 2,
             'vpadding' => 'auto',
@@ -39,11 +41,13 @@ class PDFGenerator extends PDFGeneratorCore
         $this->setPageOrientation('P', true, 40);
         $this->AddPage();
         if(strpos($this->header, 'BEZORGEN') !== false || strpos($this->header, 'BEZORGING') !== false || strpos($this->header, 'AFHALEN') !== false || strpos($this->header, 'TOEVOEGEN') !== false){
-        $link = new LinkCore;
-        $id_order = Tools::getValue('id_order');
-        $reference = Order::getUniqReferenceOf($id_order);
-
-        $this->write2DBarcode('FOL/'.Configuration::get('MODERNESMIDTHEMECONFIGURATOR_TOKEN').'/'.$reference, 'QRCODE,H', 160, 39, 40, 40, $style, 'R');
+            $link = new LinkCore;
+            if(is_null($reference)){
+                $id_order = Tools::getValue('id_order');
+                $reference = Order::getUniqReferenceOf($id_order);
+            }
+            
+            $this->write2DBarcode('FOL/'.Configuration::get('MODERNESMIDTHEMECONFIGURATOR_TOKEN').'/'.$reference, 'QRCODE,H', 160, 39, 40, 40, $style, 'R');
         }
         $this->writeHTML($this->content, true, false, true, false, '');
     }

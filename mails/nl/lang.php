c5af09296aad3b0014af098ef3c0f354ef63fabd
= <=>
2021-03-02 10:03:52 +0100

    =update from live  ijzershop.nl

--- mails/nl/lang.php
+++ mails/nl/lang.php
@@ -2,13 +2,14 @@
 
 global $_LANGMAIL;
 $_LANGMAIL = array();
+$_LANGMAIL['Order confirmation'] = 'Bestelling bevestigd';
 $_LANGMAIL['Product out of stock'] = '';
 $_LANGMAIL['Your order has been changed'] = '';
-$_LANGMAIL['New order : #%d - %s'] = '{order_name} | {total_paid} | {carrier} | {payment}';
-$_LANGMAIL['Stock coverage'] = '';
-$_LANGMAIL['New return from order #%d - %s'] = '';
+$_LANGMAIL['Order payment'] = '';
 $_LANGMAIL['Product available'] = '';
-$_LANGMAIL['Backup result'] = '';
-$_LANGMAIL['Message from %1$s %2$s'] = '';
+$_LANGMAIL['New return from order #%d - %s'] = '';
+$_LANGMAIL['New order : #%d - %s'] = '';
+$_LANGMAIL['Stock coverage'] = '';
+$_LANGMAIL['Two-factor code recovery'] = '';
 
 ?>

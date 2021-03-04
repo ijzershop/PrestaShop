51aa8ba7eb4b35f48311944ef51cfa672657a3fd
JB Stoker <jelmer@ijzershop.nl>
2021-03-04 15:18:15 +0100

    trying to add email theme submodule

---
+++ modules/autoupgrade/vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'prestashop/autoupgrade',
+  ),
+  'versions' => 
+  array (
+    'doctrine/collections' => 
+    array (
+      'pretty_version' => 'v1.3.0',
+      'version' => '1.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6c1e4eef75f310ea1b3e30945e9f06e652128b8a',
+    ),
+    'prestashop/autoupgrade' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'symfony/filesystem' => 
+    array (
+      'pretty_version' => 'v2.8.34',
+      'version' => '2.8.34.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1f4e8351e0196562f5e8ec584baeceeb8e2e92f6',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v1.35.0',
+      'version' => '1.35.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'daa657073e55b0a78cce8fdd22682fddecc6385f',
+    ),
+  ),
+);

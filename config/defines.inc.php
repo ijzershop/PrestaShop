d1508095ad6dc356a3d7de9008d65cb6355ed596
Jelmer Stoker <jelmer@ijzershop.nl>
2021-08-16 08:28:17 +0200

    =update from live nl website

--- config/defines.inc.php
+++ config/defines.inc.php
@@ -27,13 +27,13 @@
 /* Debug only */
 if (!defined('_PS_MODE_DEV_')) {
     if (isset($_SERVER["REMOTE_ADDR"]) && ($_SERVER["REMOTE_ADDR"] == '213.93.172.135' || $_SERVER["REMOTE_ADDR"] == '62.195.191.68' || $_SERVER["REMOTE_ADDR"] == '::1')) {
-        define('_PS_MODE_DEV_', true);
+        define('_PS_MODE_DEV_', false);
     } else {
         define('_PS_MODE_DEV_', false);
     }
 } else {
     if (getenv('_PS_MODE_DEV_') && isset($_SERVER["REMOTE_ADDR"]) && ($_SERVER["REMOTE_ADDR"] == '213.93.172.135' || $_SERVER["REMOTE_ADDR"] == '62.195.191.68' || $_SERVER["REMOTE_ADDR"] == '::1')) {
-        define('_PS_MODE_DEV_', true);
+        define('_PS_MODE_DEV_', false);
     } else {
         define('_PS_MODE_DEV_', false);
     }

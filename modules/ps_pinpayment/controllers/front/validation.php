71460aed2e26685ca8d5d3a8c4cae44ccca224d7
ijzershop <jelmer@ijzershop.nl>
2023-03-09 11:28:25 +0100

    update

--- modules/ps_pinpayment/controllers/front/validation.php
+++ modules/ps_pinpayment/controllers/front/validation.php
@@ -51,7 +51,7 @@ class Ps_PinpaymentValidationModuleFrontController extends ModuleFrontController
 		$currency = $this->context->currency;
 		$total = (float)$cart->getOrderTotal(true, Cart::BOTH);
 
-		$this->module->validateOrder($cart->id, Configuration::get('PS_OS_BANKPIN'), $total, 'PIN Betaling', NULL, array(), (int)$currency->id, false, $customer->secure_key);
+		$this->module->validateOrder($cart->id, Configuration::get('BANK_PIN_COMPLETE_STATE'), $total, 'PIN Betaling', NULL, array(), (int)$currency->id, false, $customer->secure_key);
 		Tools::redirect('index.php?controller=order-confirmation&id_cart='.$cart->id.'&id_module='.$this->module->id.'&id_order='.$this->module->currentOrder.'&key='.$customer->secure_key);
 	}
 }

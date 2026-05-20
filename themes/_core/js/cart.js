c8f96f4e8fda6eba3d723a4e789505c1ecb3ebf2
JB Stoker <jelmer@ijzershop.nl>
2026-05-20 13:48:32 +0200

    no message

--- themes/_core/js/cart.js
+++ themes/_core/js/cart.js
@@ -74,84 +74,84 @@ $(() => {
 
   const $body = $('body');
 
-  $body.on('click', '[data-button-action="add-to-cart"]', (event) => {
-    event.preventDefault();
-
-    const $form = $(event.currentTarget.form);
-    const query = `${$form.serialize()}&add=1&action=update`;
-    const actionURL = $form.attr('action');
-    const addToCartButton = $(event.currentTarget);
-
-    addToCartButton.prop('disabled', true);
-
-    const isQuantityInputValid = ($input) => {
-      let validInput = true;
-
-      $input.each((index, input) => {
-        const $currentInput = $(input);
-        const minimalValue = parseInt($currentInput.attr('min'), 10);
-
-        if (minimalValue && $currentInput.val() < minimalValue) {
-          onInvalidQuantity($currentInput);
-          validInput = false;
-        }
-      });
-
-      return validInput;
-    };
-
-    let onInvalidQuantity = ($input) => {
-      $input
-        .parents(prestashop.selectors.product.addToCart)
-        .first()
-        .find(prestashop.selectors.product.minimalQuantity)
-        .addClass('error');
-      $input
-        .parent()
-        .find('label')
-        .addClass('error');
-    };
-
-    const $quantityInput = $form.find('input[min]');
-
-    if (!isQuantityInputValid($quantityInput)) {
-      onInvalidQuantity($quantityInput);
-
-      return;
-    }
-
-    $.post(actionURL, query, null, 'json')
-      .then((resp) => {
-        if (!resp.hasError) {
-          prestashop.emit('updateCart', {
-            reason: {
-              idProduct: resp.id_product,
-              idProductAttribute: resp.id_product_attribute,
-              idCustomization: resp.id_customization,
-              linkAction: 'add-to-cart',
-              cart: resp.cart,
-            },
-            resp,
-          });
-        } else {
-          prestashop.emit('handleError', {
-            eventType: 'addProductToCart',
-            resp,
-          });
-        }
-      })
-      .fail((resp) => {
-        prestashop.emit('handleError', {
-          eventType: 'addProductToCart',
-          resp,
-        });
-      })
-      .always(() => {
-        setTimeout(() => {
-          addToCartButton.prop('disabled', false);
-        }, 1000);
-      });
-  });
+  // $body.on('click', '[data-button-action="add-to-cart"]', (event) => {
+  //   event.preventDefault();
+  //
+  //   const $form = $(event.currentTarget.form);
+  //   const query = `${$form.serialize()}&add=1&action=update`;
+  //   const actionURL = $form.attr('action');
+  //   const addToCartButton = $(event.currentTarget);
+  //
+  //   addToCartButton.prop('disabled', true);
+  //
+  //   const isQuantityInputValid = ($input) => {
+  //     let validInput = true;
+  //
+  //     $input.each((index, input) => {
+  //       const $currentInput = $(input);
+  //       const minimalValue = parseInt($currentInput.attr('min'), 10);
+  //
+  //       if (minimalValue && $currentInput.val() < minimalValue) {
+  //         onInvalidQuantity($currentInput);
+  //         validInput = false;
+  //       }
+  //     });
+  //
+  //     return validInput;
+  //   };
+  //
+  //   let onInvalidQuantity = ($input) => {
+  //     $input
+  //       .parents(prestashop.selectors.product.addToCart)
+  //       .first()
+  //       .find(prestashop.selectors.product.minimalQuantity)
+  //       .addClass('error');
+  //     $input
+  //       .parent()
+  //       .find('label')
+  //       .addClass('error');
+  //   };
+  //
+  //   const $quantityInput = $form.find('input[min]');
+  //
+  //   if (!isQuantityInputValid($quantityInput)) {
+  //     onInvalidQuantity($quantityInput);
+  //
+  //     return;
+  //   }
+  //
+  //   $.post(actionURL, query, null, 'json')
+  //     .then((resp) => {
+  //       if (!resp.hasError) {
+  //         prestashop.emit('updateCart', {
+  //           reason: {
+  //             idProduct: resp.id_product,
+  //             idProductAttribute: resp.id_product_attribute,
+  //             idCustomization: resp.id_customization,
+  //             linkAction: 'add-to-cart',
+  //             cart: resp.cart,
+  //           },
+  //           resp,
+  //         });
+  //       } else {
+  //         prestashop.emit('handleError', {
+  //           eventType: 'addProductToCart',
+  //           resp,
+  //         });
+  //       }
+  //     })
+  //     .fail((resp) => {
+  //       prestashop.emit('handleError', {
+  //         eventType: 'addProductToCart',
+  //         resp,
+  //       });
+  //     })
+  //     .always(() => {
+  //       setTimeout(() => {
+  //         addToCartButton.prop('disabled', false);
+  //       }, 1000);
+  //     });
+  // });
 
   $body.on('submit', '[data-link-action="add-voucher"]', (event) => {
     event.preventDefault();
